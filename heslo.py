f477172999ee993203abbfd6e6e84ed52e1069c9
Klema40 <matej.gottwald@infis.cz>
2025-05-06 15:08:28 +0200

    Create heslo.py

---
+++ heslo.py
@@ -0,0 +1,9 @@
+import random
+
+zvuky = ["haf", "mňau", "kikirikí", "béé", "kvák"]
+
+zvuk = random.choice(zvuky)
+
+index = zvuky.index(zvuk)
+
+print("Vybral jsem zvuk \"" + zvuk + "\" na indexu " + str(index))
