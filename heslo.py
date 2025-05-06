b2d30d05346469a9c67949ce4bb13a3716bfeb0f
jacobjegay <lukas.jirinec@infis.cz>
2025-05-06 15:24:25 +0200

    Update heslo.py

--- heslo.py
+++ heslo.py
@@ -1,1 +1,5 @@
-cigi
+import random
+kreatury = ["drak", "elf", "obr", "skřítek", "čaroděj"]
+v6 = random.choice(kreatury)
+index = kreatury.index (v6)
+print (f"Vybral jsem tvora {v6} na indexu {index}")
