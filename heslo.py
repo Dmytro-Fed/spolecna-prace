3907c195a140ffa044f450dd246f5e1798792251
hjidgtrjio <mykola.denysenko@infis.cz>
2025-05-06 15:12:47 +0200

    heslo.py

--- heslo.py
+++ heslo.py
@@ -1,1 +1,6 @@
-
+import random
+osobnosti = ["Einstein", "Newton", "Tesla", "Curie", "Hawking"]
+vybrana_osobnost = random.choice(osobnosti)
+v1 = vybrana_osobnost
+index = osobnosti.index(v1)
+print("Vybral jsem osobnost" + v1 + "na indexu" + str(index))
