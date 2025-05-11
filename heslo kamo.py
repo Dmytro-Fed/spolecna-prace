889c681c48bc08ebaf56d77bd3e5c59021c6796e
Dmytro-Fed <dmytro.fedoryshchak@infis.cz>
2025-05-11 23:15:41 +0200

    Delete heslo kamo.py

--- heslo kamo.py
+++
@@ -1,5 +0,0 @@
-import random
-citoslovce = ["aah","auvajs","vrr","ughh","vžuch"]
-v9 = random.choice(citoslovce)
-index = citoslovce.index (v9)
-print (f"Vybral jsem citoslovce {v9} na indexu {index}")
