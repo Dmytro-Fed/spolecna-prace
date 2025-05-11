acd3c1031336fb6cc22b61b9ac6d8dd17f1a47d1
Dmytro-Fed <dmytro.fedoryshchak@infis.cz>
2025-05-11 23:15:51 +0200

    Delete python 52 directory

--- python 52/7Ukol.py
+++
@@ -1,6 +0,0 @@
-#Ukol č. 7
-import random
-obleceni = ["Klobouk", "šála", "rukavice", "brýle", "boty" ]
-obleceniR = (random.choice(obleceni))
-v7 = obleceni.index(obleceniR)
-print("Výbral jsem oblečení ", (obleceniR), " na indexu ", (v7) )
